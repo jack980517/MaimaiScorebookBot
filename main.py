69c332c77c24ba97da55bc4aa7f5ba4051a35054
Jacqueline <jack980517@gmail.com>
2016-07-16 03:44:33 +0800

    Progress

--- main.py
+++ main.py
@@ -6,9 +6,30 @@ from pprint import pprint
 
 def handle(msg):
 	if msg['chat']['type']!="private":
-		bot.sendMessage(msg['chat']['id'],"Please manage me privately to manage your scorebook.")
+		bot.sendMessage(msg['chat']['id'],"Please message me privately to manage your scorebook.")
 	else:
-		if msg['text']=='/start':
+		if msg['text'].startswith('/start'): # Create scorebook for user
+			c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='?'",str(msg['from']['id']))
+			result=c.fetchall()
+			if result==[]:
+				c.execute("create table ? as select * from template",str(msg['from']['id']))
+				bot.sendMessage(msg['chat']['id'],"Created scorebook for @"+msg['from']['username']+" ("+msg['from']['id']+")")
+			else:
+				bot.sendMessage(msg['chat']['id'],"You already have a scorebook here.")
+		if msg['text'].startswith('!update'): # Update score
+			input=msg['text'].split('\n')
+			'''
+			msg text example:
+			Future
+			remaster
+			118250
+			4000
+			191100
+			324050
+			9
+			1
+			0
+			'''
 			
 
 # Read token from file
